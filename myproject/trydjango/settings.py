56674571dcd5a771447620dcf5c6209db63b332b
atucker2346 <anthony.tucker@bison.howard.edu>
2025-03-22 18:47:03 -0400

    s

--- myproject/trydjango/settings.py
+++ myproject/trydjango/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:/Users/kgreg/myproject/HACKATHONPROJECT/myproject/Template'],
+        'DIRS': ['/Users/aj/Documents/GitHub/HACKATHONPROJECT/myproject/Template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
